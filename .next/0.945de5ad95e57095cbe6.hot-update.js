3d6ee628a5f22e24bb90b0936a9f2b612f50c700
lusan <daslusan@gmail.com>
2018-05-11 18:55:21 +0530

    in progress

--- .next/0.945de5ad95e57095cbe6.hot-update.js
+++
@@ -1,2 +0,0 @@
-webpackHotUpdate(0,[])
-//# sourceMappingURL=0.945de5ad95e57095cbe6.hot-update.js.map
--- .next/manifest.js
+++ .next/manifest.js
@@ -90,7 +90,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "51e283bc5b035ead2ea8"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "1afd7f743362f27b8831"; // eslint-disable-line no-unused-vars
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
@@ -755,7 +755,7 @@
 /******/ 		if (__webpack_require__.nc) {
 /******/ 			script.setAttribute("nonce", __webpack_require__.nc);
 /******/ 		}
-/******/ 		script.src = __webpack_require__.p + "" + ({"0":"main.js","1":"bundles/pages/_error.js","3":"bundles/pages/index.js"}[chunkId]||chunkId) + "-" + {"0":"ef1e06ec9b6fe2c96e92","1":"9a3a532253314e1f0220","3":"c95c18fe12db91c2fb93"}[chunkId] + ".js";
+/******/ 		script.src = __webpack_require__.p + "" + ({"0":"main.js","1":"bundles/pages/_error.js"}[chunkId]||chunkId) + "-" + {"0":"592dc34a989a69d1524e","1":"9a3a532253314e1f0220"}[chunkId] + ".js";
 /******/ 		var timeout = setTimeout(onScriptComplete, 120000);
 /******/ 		script.onerror = script.onload = onScriptComplete;
 /******/ 		function onScriptComplete() {
